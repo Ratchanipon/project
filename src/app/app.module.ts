cc5b2b074708e80d29b3447176cd7741b2aa59dc
Ratchanipon <gawin15138@gmail.com>
2018-04-07 17:36:09 +0700

    เรียกค่าโปรเจค

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GetProjectProvider } from '../providers/project/get-project';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -33,7 +36,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GetProjectProvider
   ]
 })
 export class AppModule {}
--- src/pages/project/project.ts
+++ src/pages/project/project.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { GetProjectProvider } from '../../providers/project/get-project';
 
 /**
  * Generated class for the ProjectPage page.
@@ -15,7 +16,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProjectPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  projectList:any;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public project: GetProjectProvider) {
+
+              this.project.getProject().then(data => {
+                this.projectList = data;
+                console.log(this.projectList);
+                
+              })  
+
+
   }
 
   ionViewDidLoad() {
